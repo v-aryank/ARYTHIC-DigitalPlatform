0b7c9cdbffe441bfb1d560d4adae8eded23a0c63
V. Aryan Kabir <v.aryankabir007@gmail.com>
2026-09-02 09:02:29 +0600

    Added web analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import Index from "./pages/Index";
 import ResearchLabPage from "./pages/ResearchLabPage";
@@ -36,6 +37,7 @@ const App = () => (
       <BrowserRouter>
         <AnimatedRoutes />
       </BrowserRouter>
+      <Analytics />
       <SpeedInsights />
     </TooltipProvider>
   </QueryClientProvider>
